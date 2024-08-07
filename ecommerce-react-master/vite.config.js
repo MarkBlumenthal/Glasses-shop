3534e2911eb302aeb5f7121e6e1c89f75a2f5650
Mark Blumenthal <markblumenthal@live.com>
2024-08-07 17:31:52 +0300

    changes

--- ecommerce-react-master/vite.config.js
+++ ecommerce-react-master/vite.config.js
@@ -34,6 +34,53 @@
 
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { VitePWA } from 'vite-plugin-pwa';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       srcDir: 'src',
+//       filename: 'sw.js',
+//       strategies: 'injectManifest',
+//       injectManifest: {
+//         swSrc: path.resolve(__dirname, 'src/sw-src.js'),
+//       },
+//       devOptions: {
+//         enabled: true,
+//       },
+//     })
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src')
+//     }
+//   },
+//   server: {
+//     port: process.env.PORT || 3000,
+//     host: '0.0.0.0'
+//   },
+//   preview: {
+//     port: process.env.PORT || 3000,
+//     host: '0.0.0.0'
+//   },
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'index.html'),
+//       }
+//     }
+//   }
+// });
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -73,9 +120,8 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
       }
-    }
+    },
+    chunkSizeWarningLimit: 2600, // Adjust this limit if needed
   }
 });
-
-
 
