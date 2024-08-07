d363b69d2852b2cc9d317ba5fd37fe9715a33521
Mark Blumenthal <markblumenthal@live.com>
2024-08-07 14:43:46 +0300

    changes

--- ecommerce-react-master/vite.config.js
+++ ecommerce-react-master/vite.config.js
@@ -1,6 +1,6 @@
+// import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 // import path from 'path';
-// import { defineConfig } from 'vite';
 
 // export default defineConfig({
 //   plugins: [
@@ -18,18 +18,41 @@
 //   preview: {
 //     port: process.env.PORT || 3000,
 //     host: '0.0.0.0'
+//   },
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'index.html'),
+//       }
+//     }
 //   }
 // });
+
+
+
 
 
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      srcDir: 'src',
+      filename: 'sw.js',
+      strategies: 'injectManifest',
+      injectManifest: {
+        swSrc: path.resolve(__dirname, 'src/sw-src.js'),
+      },
+      devOptions: {
+        enabled: true,
+      },
+    })
   ],
   resolve: {
     alias: {
@@ -45,13 +68,9 @@ export default defineConfig({
     host: '0.0.0.0'
   },
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      }
-    }
+    outDir: 'dist'
   }
 });
+
 
 
