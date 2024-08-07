97bb6cc70a98e49c179463776f29e49da84a3ca3
Mark Blumenthal <markblumenthal@live.com>
2024-08-07 18:29:19 +0300

    changes

--- ecommerce-react-master/vite.config.js
+++ ecommerce-react-master/vite.config.js
@@ -1,105 +1,10 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src')
-//     }
-//   },
-//   server: {
-//     port: process.env.PORT || 3000,
-//     host: '0.0.0.0'
-//   },
-//   preview: {
-//     port: process.env.PORT || 3000,
-//     host: '0.0.0.0'
-//   },
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: {
-//         main: path.resolve(__dirname, 'index.html'),
-//       }
-//     }
-//   }
-// });
-
-
-
-
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-// import { VitePWA } from 'vite-plugin-pwa';
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     VitePWA({
-//       srcDir: 'src',
-//       filename: 'sw.js',
-//       strategies: 'injectManifest',
-//       injectManifest: {
-//         swSrc: path.resolve(__dirname, 'src/sw-src.js'),
-//       },
-//       devOptions: {
-//         enabled: true,
-//       },
-//     })
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src')
-//     }
-//   },
-//   server: {
-//     port: process.env.PORT || 3000,
-//     host: '0.0.0.0'
-//   },
-//   preview: {
-//     port: process.env.PORT || 3000,
-//     host: '0.0.0.0'
-//   },
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: {
-//         main: path.resolve(__dirname, 'index.html'),
-//       }
-//     }
-//   }
-// });
-
-
-
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      srcDir: 'src',
-      filename: 'sw.js',
-      strategies: 'injectManifest',
-      injectManifest: {
-        swSrc: path.resolve(__dirname, 'src/sw-src.js'),
-      },
-      devOptions: {
-        enabled: true,
-      },
-    })
   ],
   resolve: {
     alias: {
@@ -120,8 +25,13 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
       }
-    },
-    chunkSizeWarningLimit: 2600, // Adjust this limit if needed
+    }
   }
 });
+
+
+
+
+
+
 
